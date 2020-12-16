6d3f5b6faa1624e5af7fffad0c8bcecdba7035d3
ricky42613 <ricky42613@gmail.com>
2020-12-16 16:31:05 +0800

    creat structure

---
+++ graph.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "graph.h"
+
+int main(int argc,char *argv[]){
+    graph db;
+    db.word_cnt = 0; //initialize
+    
+}
---
+++ graph.h
@@ -0,0 +1,70 @@
+#define ID_LENGTH 100
+#define SOUND_LENGTH 100
+#define WORD_LENGTH 1024
+#define NODE_TABLE_LENGTH 200000
+#define EDGE_TABLE_LENGTH 200000
+
+typedef struct graph_edge graph_edge;
+typedef struct word_ferq_sound word_ferq_sound;
+typedef struct graph_node graph_node;
+typedef struct graph_node graph_node;
+typedef struct graph_edge_list graph_edge_list;
+typedef struct graph graph;
+typedef struct graph_node_list graph_node_list;
+typedef struct node_table node_table;
+typedef struct edge_table edge_table;
+typedef struct graph graph;
+
+
+struct graph_edge
+{
+    char id[ID_LENGTH];
+    int pmi;
+    graph_node *output;
+};
+
+struct graph_edge_list
+{
+    graph_edge edge;
+    struct graph_edge_list *next;
+};
+
+
+struct word_ferq_sound
+{
+    int freq;
+    char sound[SOUND_LENGTH];
+    struct word_ferq_sound *next;
+};
+
+
+struct graph_node
+{
+    char id[ID_LENGTH];
+    char word[WORD_LENGTH];
+    word_ferq_sound *word_fs;
+    graph_edge graph_edge_list;
+};
+
+struct graph_node_list
+{
+    graph_node graph_node;
+    graph_node_list *next;
+};
+
+struct node_table
+{
+    graph_node_list *node_list[NODE_TABLE_LENGTH];
+};
+
+struct edge_table
+{
+    graph_edge_list *edge_list[EDGE_TABLE_LENGTH];
+};
+
+struct graph
+{
+    edge_table edge_hash_tb;
+    node_table node_hash_tb;
+    int word_cnt;
+};
